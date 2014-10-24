9711ae5fecf141b9ba6724c53d30bfd3c0f84c2c
eleventhal <eleventhal@users.noreply.github.com>
2014-10-24 17:14:47 -0400

    Fixed bug where could not use textcomplete in the middle of the textarea and would only work at the end. Not certain why the initial function was intented to be private and not override the one in Textarea.

--- src/ie_textarea.js
+++ src/ie_textarea.js
@@ -17,7 +17,7 @@
     // --------------
 
     select: function (value, strategy) {
-      var pre = this._getTextFromHeadToCaret();
+      var pre = this.getTextFromHeadToCaret();
       var post = this.el.value.substring(pre.length);
       var newSubstr = strategy.replace(value);
       if ($.isArray(newSubstr)) {
@@ -34,7 +34,7 @@
       range.select();
     },
 
-    _getTextFromHeadToCaret: function () {
+    getTextFromHeadToCaret: function () {
       this.el.focus();
       var range = document.selection.createRange();
       range.moveStart('character', -this.el.value.length);
