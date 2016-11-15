996bc7cea57554a4d1de75f7a8fabe3659eeb75c
Jun Omae <jun66j5@gmail.com>
2016-11-15 20:00:58 +0900

    Skip search after pressing shift, ctrl, alt, pageup and pagedown keys

--- src/adapter.js
+++ src/adapter.js
@@ -116,6 +116,11 @@
       switch (clickEvent.keyCode) {
         case 9:  // TAB
         case 13: // ENTER
+        case 16: // SHIFT
+        case 17: // CTRL
+        case 18: // ALT
+        case 33: // PAGEUP
+        case 34: // PAGEDOWN
         case 40: // DOWN
         case 38: // UP
         case 27: // ESC
