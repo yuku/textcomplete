1e7d781c9a3d5b3be8bae5dfb5fd9f76a23e9de9
beuseli <pieter.beuselinck@infohos.be>
2017-08-10 11:13:37 +0200

    On IE: Correct alignment of jquery-textcomplete-dropdown when scrolling down in iframe
    On IE: When the jquery-textcomplete was placed in an iframe (e.g. ckeditor) the jquery-textcomplete dropdown was not aligned correctly when the user scrolled down in the textarea.
    
    This was caused by the fact that scrollTop (of the body-element inside the iframe) is used to calculate the correct position of the dropdown. However on IE scrollTop of body is always zero. This a known issue: https://stackoverflow.com/questions/2717252/document-body-scrolltop-is-always-0-in-ie-even-when-scrolling
    
    This was solved by getting the scrollTop of the html-element inside the iframe, which works for all browsers, including IE.

--- dist/jquery.textcomplete.js
+++ dist/jquery.textcomplete.js
@@ -1285,7 +1285,7 @@ if (typeof jQuery === 'undefined') {
         position.top += iframePosition.top;
         position.left += iframePosition.left;
         //subtract scrollTop from element in iframe
-        position.top -= this.$el.scrollTop(); 
+        position.top -= $(this.completer.$iframe[0].contentWindow.document).scrollTop(); //We need to get the scrollTop of the html-element inside the iframe and not of the body-element, because on IE the scrollTop of the body-element (this.$el) is always zero.
       }
       
       $node.remove();
