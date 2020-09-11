f964536df899f89782d9daa229d5988489930d4d
Prakash Venkat <venkatraman.prakash@gmail.com>
2020-09-11 10:19:26 -0700

    Handle highlight with JS alone

--- packages/textcomplete-core/src/Dropdown.ts
+++ packages/textcomplete-core/src/Dropdown.ts
@@ -78,7 +78,7 @@ export class Dropdown extends EventEmitter {
     this.items = searchResults
       .slice(0, this.option.maxCount || DEFAULT_DROPDOWN_MAX_COUNT)
       .map(
-        (r, index) => new DropdownItem(this, index, r, this.option?.item || {})
+        (r, index) => new DropdownItem(this, index, r, this.option?.item || {}, this.activate.bind(this, index))
       )
     this.setStrategyId(searchResults[0])
       .renderEdge(searchResults, "header")
@@ -183,13 +183,16 @@ export class Dropdown extends EventEmitter {
 
   activate(index: number): this {
     if (this.activeIndex !== index) {
-      if (this.activeIndex != null) {
-        this.items[this.activeIndex].deactivate()
-      }
-      this.activeIndex = index
-      this.items[index].activate()
+      this.deactivateAll();
+      this.activeIndex = index;
+      this.items[index].activate();
     }
     return this
+  }
+
+  deactivateAll(): this {
+    this.items.forEach((i) => i.deactivate());
+    return this;
   }
 
   isShown(): boolean {
@@ -302,7 +305,8 @@ class DropdownItem {
     private readonly dropdown: Dropdown,
     private readonly index: number,
     public readonly searchResult: SearchResult<unknown>,
-    private readonly props: DropdownItemOption
+    private readonly props: DropdownItemOption,
+    private readonly activationHandler: any
   ) {
     this.className = this.props.className || DEFAULT_DROPDOWN_ITEM_CLASS_NAME
     this.activeClassName =
@@ -316,8 +320,9 @@ class DropdownItem {
     span.innerHTML = this.searchResult.render()
     li.appendChild(span)
 
-    li.addEventListener("mousedown", this.onClick)
-    li.addEventListener("touchstart", this.onClick)
+    li.addEventListener("mousedown", this.onClick);
+    li.addEventListener("touchstart", this.onClick);
+    li.addEventListener("mouseenter", activationHandler);
 
     this.el = li
   }
