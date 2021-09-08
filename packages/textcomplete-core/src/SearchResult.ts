0ee5e68ec38c391fd59b3c8c4d3f8c4811c038b6
Yuta Ohigashi <ohigashi.yuta@a-tm.co.jp>
2021-09-08 12:03:23 +0900

    Replace only needed range in CodeMirror

--- packages/textcomplete-core/src/SearchResult.ts
+++ packages/textcomplete-core/src/SearchResult.ts
@@ -10,23 +10,46 @@ export class SearchResult<T = unknown> {
     private readonly strategy: Strategy<T>
   ) {}
 
-  replace(beforeCursor: string, afterCursor: string): [string, string] | void {
+  getReplacementData(beforeCursor: string): {
+    start: number
+    end: number
+    beforeCursor: string
+    afterCursor: string
+  } | null {
     let result = this.strategy.replace(this.data)
-    if (result == null) return
+    if (result == null) return null
+
+    let afterCursor = ""
     if (Array.isArray(result)) {
-      afterCursor = result[1] + afterCursor
+      afterCursor = result[1]
       result = result[0]
     }
     const match = this.strategy.match(beforeCursor)
-    if (match == null || match.index == null) return
+    if (match == null || match.index == null) return null
     const replacement = result
       .replace(MAIN, match[0])
       .replace(PLACE, (_, p) => match[parseInt(p)])
+
+    return {
+      start: match.index,
+      end: match.index + match[0].length,
+      beforeCursor: replacement,
+      afterCursor: afterCursor,
+    }
+  }
+
+  replace(beforeCursor: string, afterCursor: string): [string, string] | void {
+    const replacement = this.getReplacementData(beforeCursor)
+
+    if (replacement === null) return
+
+    afterCursor = replacement.afterCursor + afterCursor
+
     return [
       [
-        beforeCursor.slice(0, match.index),
-        replacement,
-        beforeCursor.slice(match.index + match[0].length),
+        beforeCursor.slice(0, replacement.start),
+        replacement.beforeCursor,
+        beforeCursor.slice(replacement.end),
       ].join(""),
       afterCursor,
     ]
